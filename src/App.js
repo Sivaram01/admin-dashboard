78322d06cd9bf6a542837136e9a54649e83dc4d7
Sivaram01 <sivaram.v10@gmail.com>
2021-12-17 21:14:11 +0530

    admin-dashoard with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './componets/Navbar'
+import Sidebar from './componets/Sidebar';
+import Header from './componets/header'
+import Chart from './componets/chart.js';
+import Main from './componets/main.js'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Sidebar/>
+      <Navbar/>
+      <Header/>
+      <Chart/>
+      <Main/>
     </div>
   );
 }
 
-export default App;
+
+
